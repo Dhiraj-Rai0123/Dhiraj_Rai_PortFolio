528bee7751874e3eb6c8173025e743461f79d9a1
Dhiraj Rai <drajrai01@gmail.com>
2026-08-18 23:35:24 +0545

    woring with codes

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -2,14 +2,13 @@ import React from 'react'
 
 const Skills = () => {
   return (
-  <> 
-  <div className="h-100 w-100 border bg-gradient from-blue-200 via-green-300 to-blue-200">
-    <div className="">
+    <>
+    <div>
+      <div> 
         
+      </div>
     </div>
-  </div>
-  
-  </>
+    </>
   )
 }
 
